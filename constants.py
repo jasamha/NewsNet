5195692604c1fc0ce8eb8061529741db1533d5da
jasamha <zhangsh86398@163.com>
2018-06-05 11:48:09 +0800

    添加了对变量命名的管理

---
+++ constants.py
@@ -0,0 +1,15 @@
+"""
+ *  @ 创建者      zsh
+ *  @ 创建时间    18-6-2 上午9:40
+ *  @ 创建描述    存储常量字符串
+ *  
+"""
+# views_user.py
+
+# 图形验证码的session键
+IMAGE_VERIFY = 'image_verify'
+# 短信验证码的session键
+SMS_VERIFY = "sms_verify"
+MOBILE = "mobile"
+PASSWORD = "password"
+USER_ID = "user_id"
